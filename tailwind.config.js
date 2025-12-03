78f75ffe42388bc6c18b36d264e97bd108be8abe
yeongtye-creator <yeongtye@gmail.com>
2025-12-04 00:12:40 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          950: '#020617',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
